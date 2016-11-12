bfbc3c5de6024c863c82212c0c7180b457bd9bef
Ryan House <Portuga@Ryans-MacBook-Pro.local>
2016-11-12 11:00:27 -0800

    Killed MeetUp API =(

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,29 +5,6 @@
 
 //<script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
 
-// &radius=1&category=253&order=members4
-
-var meetUpQueryUrl = "https://api.meetup.com/find/groups?";
-var meetUpKey = "&key=3a117fb243a676d1151958167a3168";
-var meetUpZipcode = "zip=" + 90210;
-var radius = 1;
-var meetUpRadius = "&radius=" + radius;
-
-var fullMeetUpQueryUrl = meetUpQueryUrl + meetUpZipcode + meetUpRadius + meetUpKey;
-
-
-$.ajax({
-	url: fullMeetUpQueryUrl,
-	method: "GET"
-})
-.done(function(response){
-	// var results = response.dataset.data;
-	console.log(response);
-	// console.log(results[0][1]); //first # is for the month, the lower the # the more recent. second # is the date (0) or Price (1)
-});
-
-
-
 var houseQueryUrl = "https://www.quandl.com/api/v3/datasets/ZILL/";
 var houseKey = "api_key=y2xh6kV4KLrYCNGRJmSj"
 var numResults = 10; //number
@@ -62,14 +39,7 @@ var fullQueryCity = houseQueryUrl + areaType.city + city + housingType.medianRen
 $.ajax({
 	url: fullQueryZipcode,
 	method: "GET",
-	contentType: "text/plain",
-	crossDomain: true,
-	xhrFields: {
-		withCredentials: false
-	},
-	headers: {
-		"Access-Control-Allow-Origins": "moveto.heroku.com",
-	}
+	
 })
 .done(function(response){
 	// var results = response.dataset.data;
